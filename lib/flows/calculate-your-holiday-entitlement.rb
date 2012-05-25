9ab68dc411bbfe11f909eea3ef6d5f95f2a74872
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2012-05-25 16:01:48 +0100

    Next stage of full time and figure out indenting format

--- lib/flows/calculate-your-holiday-entitlement.rb
+++ lib/flows/calculate-your-holiday-entitlement.rb
@@ -1,10 +1,15 @@
 status :draft
 
 multiple_choice :employment_status? do
-  option :full_time => :full_time_worked?
-  option :part_time => :part_time_worked?
-  option :casual_or_irregular => :casual_or_irregular_hours?
-  option :annualised_hours => :annualised_hours?
-  option :compressed_hours => :compressed_hours?
-  option :shift_worker => :shift_worker_basis?
+  option "full-time" => :full_time_worked?
+  option "part-time" => :part_time_worked?
+  option "casual-or-irregular-hours" => :casual_or_irregular_hours?
+  option "annualised-hours" => :annualised_hours?
+  option "compressed-hours" => :compressed_hours?
+  option "shift-worker" => :shift_worker_basis?
+end
+
+multiple_choice :full_time_worked? do
+  option :full_time_year => :full_time_year
+  option :full_time_part => :full_time_part
 end

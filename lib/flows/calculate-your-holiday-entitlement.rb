55bf6fb524732a2a38efd538d4af4058eaa476be
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2012-05-28 11:00:30 +0100

    Half of part-time flow

--- lib/flows/calculate-your-holiday-entitlement.rb
+++ lib/flows/calculate-your-holiday-entitlement.rb
@@ -12,7 +12,7 @@ end
 
 multiple_choice :full_time_worked? do
   option "year" => :full_time_year
-  option "part-year" => :full_time_part
+  option "part-year" => :full_time_part_leaving
 end
 
 multiple_choice :full_time_year do
@@ -20,5 +20,27 @@ multiple_choice :full_time_year do
   option "more-than-5" => :done_full_time_more_than_5
 end
 
+date_question :full_time_part_leaving do
+  save_input_as :start_date
+  calculate :days_employed do
+
+  end
+
+end
+
+multiple_choice :part_time_worked? do
+  option "year" => :part_time_year_days_worked?
+  option "part-year" => :part_time_part_year
+end
+
+value_question :part_time_year_days_worked? do
+  next_node :done_part_time_year
+  calculate :part_time_holiday_entitlement do
+    # TODO: clarify exact rounding with Simon Kaplan
+    responses.last.to_f * 5.6
+  end
+end
+
 outcome :done_full_time_5_days
 outcome :done_full_time_more_than_5
+outcome :done_part_time_year
